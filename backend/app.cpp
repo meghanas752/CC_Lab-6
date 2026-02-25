29813721d89a5290fe65d01b6157041c919b1dea
Meghana <meghana0039@gmail.com>
2026-02-25 20:34:48 +0000

    Fixed backend HTTP server

--- backend/app.cpp
+++ backend/app.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
-#include <cstdlib>
+#include <unistd.h>
+#include <cstring>
+#include <arpa/inet.h>
 
 int main() {
-    std::cout << "Served by backend container" << std::endl;
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    while (true) {
+        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+
+        std::string response =
+            "HTTP/1.1 200 OK\nContent-Type: text/plain\n\n"
+            "Served by backend: " + std::to_string(getpid());
+
+        send(new_socket, response.c_str(), response.length(), 0);
+        close(new_socket);
+    }
+
     return 0;
 }
